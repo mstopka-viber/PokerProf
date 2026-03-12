c97ebbc15db1d5565e2ae8f5d87f6e1a768ada74
mstopka-viber <mstopka@hotmail.com>
2026-03-12 15:45:31 -0700

    restore working version

---
+++ client/public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'pokerprof-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
